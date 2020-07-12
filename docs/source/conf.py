9f72cdcb10cd33b14a85a4904448aa80c1a4f4c5
Guillaume Parent <gp@gparent.net>
2020-07-12 07:09:05 +0000

    Bump version: 0.1.0 -> 0.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2020, Guillaume Parent'
 author = 'Guillaume Parent'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md") as fi:
 setup(
 
     name="f1-2020-telemetry",
-    version="0.1.0",
+    version="0.1.1",
 
     author="Guillaume Parent",
     author_email="gp@gparent.net",
