d1282c0080bfd9a7f2814c57cc478edc00efd528
Guillaume Parent <gp@gparent.net>
2020-09-16 05:53:26 +0000

    Add tests for speedtrap

---
+++ f1_2020_telemetry/tests/test_packet_event_speedtrap.py
@@ -0,0 +1,31 @@
+from f1_2020_telemetry.packets import unpack_udp_packet
+
+
+def is_speedtrap(event_packet):
+    return (
+        event_packet.header.packetId == 3
+        and event_packet.eventStringCode.decode() == "SPTP"
+    )
+
+
+def verify_speedtrap_members(
+    speedtrap_details,
+    speed: int,
+    vehicle: int,
+):
+    assert speedtrap_details.speed == speed
+    assert speedtrap_details.vehicleIdx == vehicle
+
+
+def test_packet_event_speedtrap():
+    packet = unpack_udp_packet(
+        b"\xe4\x07\x01\t\x01\x03J\x15Dl\n\xb5\xa77\xc8\x9e\x83C\x1b,\x00\x00\xff\xffSPTP\x038Y\xa6C\x00\x00"
+    )
+
+    assert is_speedtrap(packet)
+
+    verify_speedtrap_members(
+        speedtrap_details=packet.eventDetails.speedTrap,
+        speed=332.697021484375,
+        vehicle=3,
+    )
